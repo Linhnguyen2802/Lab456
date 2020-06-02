0fb636a1641e3308585f50b18da8751a83f8c01e
Linhnguyen2802 <nguyenvulinh2802@gmail.com>
2020-06-02 12:31:16 +0700

    part1

--- BigSchool/BigSchool/Controllers/CoursesController.cs
+++ BigSchool/BigSchool/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using BigSchool.Models;
 using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,31 @@ namespace BigSchool.Controllers
         {
             _dbContext = new ApplicationDbContext();
         }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+
+        {
+             if (!ModelState.IsValid)
+             {
+                 viewModel.Categories = _dbContext.Categories.ToList();
+                 return View("Create", viewModel);
+             }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                 CategoryId = viewModel.Category,
+                 Place = viewModel.Place
+
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -24,5 +49,10 @@ namespace BigSchool.Controllers
             };
             return View(viewModel);
         }
+
+       
+
+
+
     }
 }
